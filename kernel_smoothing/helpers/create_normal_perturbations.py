8966f68ff13211a13ade5ccac109da0dcbd5f496
ashwinravi <raviashwin87@gmail.com>
2023-09-06 17:30:03 +0530

    Adding more documentation to the create_normal_perturbations function.

--- kernel_smoothing/helpers/create_normal_perturbations.py
+++ kernel_smoothing/helpers/create_normal_perturbations.py
@@ -8,9 +8,12 @@ def create_normal_perturbations(point_list, variance=1):
     Here we generate a normal perturbation , which means that each point is disturbed in such a way that
     it is replaced by a value picked randonly from a normal distribution centered at that point with the
     specified variance.
-    :param point_list:
-    :param variance:
-    :return:
+
+    Arguments:
+    ----------
+    :param point_list: The input point list
+    :param variance: The variance to be used in the normal distribution that is used in random sampling.
+    :return: A list of perturbed values.
     """
 
     return [np.random.normal(point, variance) for point in point_list]
